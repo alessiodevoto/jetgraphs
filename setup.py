de9fb0dd82c8a3a709143e9c8d9dd357eb33bfce
alessiodevoto <devoto.alessio@gmail.com>
2022-09-26 11:53:51 +0200

    update requiremetns.txt

--- setup.py
+++ setup.py
@@ -5,24 +5,19 @@ setup(
     name='jetgraphs',
     url='https://github.com/alessiodevoto/jetgraphs.git',
     author='Alessio Devoto',
-    # Needed to actually package something
     packages=['jetgraphs'],
-    # Needed for dependencies
     install_requires=[
         'numpy',
-        'matplotlib==3.2.2',
-        'networkx==2.6.3',
-        'pytorch_lightning==1.7.1',
-        'scikit-learn==1.0.2',
-        'scipy==1.7.3',
-        'torch-geometric==2.0.4',
-        'torchmetrics==0.9.3',
-        'tqdm'],
-    # *strongly* suggested for sharing
+        'matplotlib>=3.2.2',
+        'networkx',
+        'pytorch_lightning',
+        'scikit-learn',
+        'scipy',
+        'torchmetrics',
+        'tqdm'], # TODO add here torch geometric!
     version='0.1',
     # The license can be anything you like
     # license='MIT',
     description='A package to manipulate and plot jetgraphs',
-    # We will also need a readme eventually (there will be a warning)
     long_description=open('README.txt').read(),
 )
