0b49115cae7725af1d6c001f2b7be7eeec3548e9
alessiodevoto <devoto.alessio@gmail.com>
2022-09-26 11:45:27 +0200

    added requirements

--- setup.py
+++ setup.py
@@ -4,17 +4,25 @@ setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='jetgraphs',
     url='https://github.com/alessiodevoto/jetgraphs.git',
-    author='John Ladan',
-    # author_email='jladan@uwaterloo.ca',
+    author='Alessio Devoto',
     # Needed to actually package something
-    # packages=['measure'],
+    packages=['jetgraphs'],
     # Needed for dependencies
-    # install_requires=['numpy'],
+    install_requires=[
+        'numpy',
+        'matplotlib==3.2.2',
+        'networkx==2.6.3',
+        'pytorch_lightning==1.7.1',
+        'scikit-learn==1.0.2',
+        'scipy==1.7.3',
+        'torch-geometric==2.0.4',
+        'torchmetrics==0.9.3',
+        'tqdm'],
     # *strongly* suggested for sharing
-    # version='0.1',
+    version='0.1',
     # The license can be anything you like
     # license='MIT',
-    description='An example of a python package from pre-existing code',
+    description='A package to manipulate and plot jetgraphs',
     # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
+    long_description=open('README.txt').read(),
 )
