a410f82e6f5dbc2263be086dc7757cbc000aa3cf
alessiodevoto <devoto.alessio@gmail.com>
2022-10-12 11:19:44 +0200

    only new function to plot jetgraph

--- jetgraphs/utils.py
+++ jetgraphs/utils.py
@@ -13,80 +13,6 @@ import seaborn as sns
 import os
 
 
-def plot_jet_graph(g, node_distance=0.3, display_energy_as='colors', ax=None, figsize=(5, 5), elev=30, angle=0):
-    """
-    Display graph g, assuming 4 attributes per node and optimal distance between node and node size.
-    TODO complete this comment
-    :parameter g: graph to plot Data object
-    :parameter node_distance :
-    :parameter diplay_energy_as : how the energy should be displayed options are ['colors', 'size', 'colors_and_size']
-    :parameter ax : matplotlib axis
-    """
-
-    print("WARNING: this function will be removed soon")
-
-    plt.style.use('ggplot')
-    # The graph to visualize
-    G = to_networkx(g, node_attrs=['x'])
-
-    # Remove last coordinate, i.e. energy, and store it in other attribute field
-    for node_idx in G.nodes():
-        G.nodes[node_idx]['energy'] = G.nodes[node_idx]['x'][-1]
-        G.nodes[node_idx]['x'] = G.nodes[node_idx]['x'][0:3]
-
-    # 3d spring layout
-    pos = nx.spring_layout(G, k=node_distance, dim=3, seed=779)
-    # Extract node and edge positions from the layout
-    node_xyz = np.array([pos[v] for v in sorted(G)])
-    edge_xyz = np.array([(pos[u], pos[v]) for u, v in G.edges()])
-
-    energies = np.array([G.nodes[node_idx]['energy'] for node_idx in G.nodes])
-
-    # Create the 3D figure
-    if not ax:
-        fig = plt.figure(figsize=figsize)
-        ax = fig.add_subplot(111, projection="3d")
-    else:
-        fig = ax.get_figure()
-
-    # Plot the nodes - alpha is scaled by "depth" automatically
-    if display_energy_as == 'colors':
-        ax.scatter(*node_xyz.T, c=energies, s=50, ec="w")
-    elif display_energy_as == 'size':
-        ax.scatter(*node_xyz.T, s=energies / np.linalg.norm(energies) * 1000, ec="w")
-    elif display_energy_as == 'colors_and_size':
-        ax.scatter(*node_xyz.T, c=energies, s=energies / np.linalg.norm(energies) * 1000, ec="w")
-    else:
-        ax.scatter(*node_xyz.T, s=50, ec="w")
-
-    # Plot the edges
-    for vizedge in edge_xyz:
-        ax.plot(*vizedge.T, color="tab:gray")
-
-    def _format_axes(ax):
-        # Turn gridlines off
-        #ax.grid(False)
-        # Suppress tick labels
-        #for dim in (ax.xaxis, ax.yaxis):
-            #dim.set_ticks([])
-        # ax.zaxis.set_ticks([1,2,3,4])
-        # Set axes labels
-        ax.set_xlabel("η")
-        ax.set_ylabel("φ")
-        ax.set_zlabel("l")
-        
-
-    _format_axes(ax)
-
-    # Set the initial view
-    ax.view_init(elev, angle)
-
-    #plt.savefig(f'/Users/alessiodevoto/projects/graph/{angle}.png')
-    #plt.close('all')
-
-
-    
-
 def plot_jet_graph2(g, save=False, angle=30, elev=10, ax=None, color_layers=True, energy_is_size=True, figsize=(5,5), save_to_path=False, **kwargs):
     """
     Display graph g, assuming 4 attributes (eta, phi, layer, energy) per node and optimal distance between node and node size.
@@ -244,7 +170,7 @@ def plot_dataset_info(df: DataFrame, title: str, include_cols : Iterable = False
     axs = [axs]
 
   # Distributions of column fields.
-  for i,col in enumerate(cols_to_plot):
+  for i, col in enumerate(cols_to_plot):
     y = [df_signal[col],  df_noise[col]] if separate_classes else df[col] 
     axs[i].set_xlabel(col)
     axs[i].set_ylabel('# graphs')
